aee8ac4188870f1c82d709375b893654bf9e94a6
Nico Rehwaldt <git_nikku@nixis.de>
2016-12-07 10:58:58 +0100

    test(backoff): ensure backoff works, too

---
+++ test/backoff-spec.js
@@ -0,0 +1,117 @@
+var fs = require('fs');
+
+var extend = require('xtend');
+
+var Workers = require('../');
+
+var Logger = require('../lib/logger');
+var Backoff = require('../lib/backoff');
+
+var EngineApi = require('./engine/api');
+
+
+function delay(seconds, fn) {
+  return setTimeout(fn, seconds * 1000);
+}
+
+function log(worker, context) {
+
+  return {
+    worker: worker,
+    context: {
+      topicName: context.topicName,
+      activityId: context.activityId,
+      processInstanceId: context.processInstanceId
+    }
+  };
+}
+
+
+describe('backoff', function() {
+
+  // slow tests...
+  this.timeout(10000);
+
+
+  var engineUrl = 'http://localhost:8080/engine-rest';
+
+  var engineApi = new EngineApi(engineUrl);
+
+
+  var workers, deployment;
+
+  beforeEach(function(done) {
+
+    var inputStream = fs.createReadStream(__dirname + '/process.bpmn');
+
+    engineApi.deploy(inputStream, function(err, newDeployment) {
+
+      if (err) {
+        return done(err);
+      }
+
+      deployment = newDeployment;
+
+      done();
+    });
+
+  });
+
+  afterEach(function(done) {
+    if (workers) {
+
+      workers.shutdown(function() {
+        var registeredWorkers = workers.workers;
+
+        Object.keys(registeredWorkers).forEach(function(topic) {
+          registeredWorkers[topic].remove();
+        });
+      });
+    }
+
+    if (deployment) {
+      engineApi.undeploy(deployment, done);
+    }
+  });
+
+
+  describe('task execution', function() {
+
+    it('should work with backoff', function(done) {
+
+      var trace = [];
+
+      var idx = 0;
+
+      workers = Workers(engineUrl, {
+        workerId: 'test-worker',
+        use: [ Logger, Backoff ]
+      });
+
+      workers.registerWorker('work:A', function(context, callback) {
+        trace.push('work:A');
+
+        callback(null);
+      });
+
+
+      for (var i = 0; i < 3; i++) {
+        engineApi.startProcessByKey('TestProcess', function() { });
+      }
+
+      delay(3, function() {
+
+        expect(trace).to.eql([
+          'work:A',
+          'work:A',
+          'work:A'
+        ]);
+
+        done();
+      });
+
+    });
+
+  });
+
+});
