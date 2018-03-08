0f05d0bddba2222ba3e0a10ed6b4bbbe000b7f03
Nico Rehwaldt <git_nikku@nixis.de>
2018-03-08 21:05:21 +0100

    feat(backoff): be able to configure maxActiveTasks
    Related to #11

--- test/backoff-spec.js
+++ test/backoff-spec.js
@@ -97,4 +97,78 @@ describe('backoff', function() {
     ]);
   });
 
+
+  it('should adjust maxTasks', async function() {
+
+    // given
+    for (var i = 0; i < 4; i++) {
+      await engineApi.startProcessByKey('TestProcess');
+    }
+
+    var trace = [];
+
+    var backoffTrace = [];
+
+    workers = Workers(engineUrl, {
+      pollingDelay: 0,
+      pollingInterval: 500,
+      use: [
+        [ Backoff, { maxActiveTasks: 1 } ],
+        Logger
+      ]
+    });
+
+    workers.on('backoff:updatePollingInterval', function(newInterval, currentInterval, reason) {
+
+      expect(newInterval).to.exist;
+      expect(currentInterval).to.exist;
+      expect(reason).to.exist;
+
+      backoffTrace.push([ reason, newInterval ]);
+    });
+
+
+    workers.on('backoff:updateMaxTasks', function(newMaxTasks, currentMaxTasks, reason) {
+
+      expect(newMaxTasks).to.exist;
+      expect(currentMaxTasks).to.exist;
+      expect(reason).to.exist;
+
+      backoffTrace.push([ reason, newMaxTasks ]);
+    });
+
+    workers.registerWorker('work:A', async function(context) {
+      // when
+      trace.push('work:A');
+
+      await delay(.5);
+    });
+
+    await delay(4);
+
+    await workers.shutdown();
+
+    // then
+    expect(trace).to.eql([
+      'work:A',
+      'work:A',
+      'work:A',
+      'work:A'
+    ]);
+
+    expect(backoffTrace).to.eql([
+      [ 'max tasks fetched', 0 ],
+      [ 'task started', 0 ],
+      [ 'max tasks reached', 500 ],
+      [ 'task started', 0 ],
+      [ 'task completed', 2 ],
+      [ 'max tasks fetched', 0 ],
+      [ 'task started', 0 ],
+      [ 'max tasks reached', 500 ],
+      [ 'task started', 0 ],
+      [ 'task completed', 2 ]
+    ]);
+
+  });
+
 });
