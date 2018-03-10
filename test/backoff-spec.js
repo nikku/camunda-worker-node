eacb75af0e345d34bf2fbf2a6b39caf813106456
Nico Rehwaldt <git_nikku@nixis.de>
2018-03-10 18:19:34 +0100

    chore(project): bump run-camunda version

--- test/backoff-spec.js
+++ test/backoff-spec.js
@@ -15,7 +15,7 @@ var {
 describe('backoff', function() {
 
   // slow tests...
-  this.timeout(30000);
+  this.timeout(15000);
 
 
   var engineUrl = 'http://localhost:8080/engine-rest';
--- test/workers-spec.js
+++ test/workers-spec.js
@@ -19,7 +19,7 @@ var {
 describe('workers', function() {
 
   // slow tests...
-  this.timeout(30000);
+  this.timeout(15000);
 
 
   var engineUrl = 'http://localhost:8080/engine-rest';
